674b23862e8b077af3845728719f0571ace7d4bc
stphnrosa <stphn.rosa@gmail.com>
2025-05-09 21:55:42 -0400

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+
+
+export default function App() {
+    // const apiKey= import.meta.env.VITE_API_KEY;
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Homepage} />
+    </Routes>
+    </>
+    <div>App</div>
+  )
+}
